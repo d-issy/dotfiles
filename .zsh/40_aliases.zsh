885ef5e45258bcb19f9a7a172cfbb561dcb96ef6
しぇぱっち <shepabashi@gmail.com>
2017-11-16 23:58:14 +0900

    add grc and exa

--- .zsh/40_aliases.zsh
+++ .zsh/40_aliases.zsh
@@ -1,9 +1,26 @@
 # ls
-alias ls="ls" # color for darwin
-alias la="ls -A"
-alias ll="ls -l"
-alias lla="ls -lA"
-alias l1="ls -1"
+if [[ -x `which exa` ]]; then
+    alias ls="exa"
+    alias la="exa -a"
+    alias ll="exa -l"
+    alias lla="exa -la"
+    alias l1="exa -1"
+else
+    alias la="ls -A"
+    alias ll="ls -l"
+    alias lla="ls -lA"
+    alias l1="ls -1"
+fi
+
+# grc
+if [[ -x `which grc` ]]; then
+    alias diff="grc diff"
+    alias netstat="grc netstat"
+    alias ping="grc ping"
+    alias ps="grc ps"
+    alias tail="grc tail"
+    alias traceroute="grc traceroute"
+fi
 
 # tree
 alias tree="tree -NC"
