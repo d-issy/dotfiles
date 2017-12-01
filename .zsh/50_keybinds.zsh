4ee597f9210c8e0aa427cc067c273238aad01586
しぇぱっち <shepabashi@gmail.com>
2017-12-02 02:58:47 +0900

    add menu select

--- .zsh/50_keybinds.zsh
+++ .zsh/50_keybinds.zsh
@@ -4,3 +4,10 @@ bindkey '^U' cdup
 bindkey '^R' history-selection
 bindkey '^J' self-insert
 bindkey '^@' ghq-src
+
+# menu select
+zmodload -i zsh/complist
+bindkey -M menuselect '^h' vi-backward-char
+bindkey -M menuselect '^j' vi-down-line-or-history
+bindkey -M menuselect '^k' vi-up-line-or-history
+bindkey -M menuselect '^l' vi-forward-char
