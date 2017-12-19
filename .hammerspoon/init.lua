6ef53faad258c2260788516229fdfd194ab1e3e1
しぇぱっち <shepabashi@gmail.com>
2017-12-19 09:15:23 +0900

    automate proxy setting by using hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -97,6 +97,42 @@ local eikanaEventTap = hs.eventtap.new({
 }, eikanaEvent)
 eikanaEventTap:start()
 
+-- wifiWatcher
+local netConfig = hs.network.configuration.open()
+local proxy = false
+local interface = 'en0'
+
+local function setProxy()
+    proxy = true
+    netConfig:setLocation('school')
+end
+
+local function unsetProxy()
+    proxy = false
+    netConfig:setLocation('local')
+end
+
+local wifiWatcher = hs.wifi.watcher.new(function(watcher, message, _) 
+    if message ~= 'SSIDChange' then
+        return
+    end
+
+    local nextSSID = hs.wifi.currentNetwork(interface)
+    if nextSSID == 'KIT-WLAP2' and (not proxy) then
+        setProxy()
+    elseif nextSSID ~= 'KIT-WLAP2' and proxy  then
+        unsetProxy()
+    end
+end)
+
+if hs.wifi.currentNetwork(interface) == 'KIT-WLAP2' then
+    setProxy()
+else
+    unsetProxy()
+end
+
+wifiWatcher:start()
+
 -- App Switcher: Vivaldi
 hs.hotkey.bind({'cmd', 'ctrl'}, 'V', function()
     hs.application.launchOrFocus('Vivaldi')
