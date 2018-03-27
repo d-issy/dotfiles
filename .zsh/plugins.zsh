74dc849613a9aa48ca3295711182baca418ebe86
しぇぱっち <shepabashi@gmail.com>
2018-03-28 00:17:20 +0900

    update plugins

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,5 +1,7 @@
+if type 'zplug' > /dev/null 2>&1; then
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 zplug "changyuheng/zsh-interactive-cd"
+fi
