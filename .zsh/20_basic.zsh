338df2b845f561c19a143c600bde05250816e78f
しぇぱっち <shepabashi@gmail.com>
2018-04-15 14:49:27 +0900

    ignore match case in zsh completion

--- .zsh/20_basic.zsh
+++ .zsh/20_basic.zsh
@@ -48,6 +48,7 @@ zstyle ':completion:*' verbose yes
 zstyle ':completion:*' format '%B%d%b'
 zstyle ':completion:*:warnings' format 'No matches for: %d'
 zstyle ':completion:*' group-name ''
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 zstyle ':completion:*' menu select interactive
 setopt menucomplete
