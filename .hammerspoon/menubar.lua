964c01c022111d08ff904150b4d7b501d0cd4256
しぇぱっち <shepabashi@gmail.com>
2018-09-17 16:30:34 +0900

    improve menu updater

--- .hammerspoon/menubar.lua
+++ .hammerspoon/menubar.lua
@@ -1,22 +1,15 @@
 local max = hs.screen.primaryScreen():frame()
 cv = hs.canvas.new{x = 0, y = max.h - 27, w = max.w, h = 20}
+cv:behavior({"canJoinAllSpaces"})
 
 menuUpdater = hs.timer.doEvery(1, function()
-    --
     local date = os.date('%m/%d %H:%M')
-    --
-    local volumeCmd = io.popen("osascript -e 'output volume of (get volume settings)'")
-    local volume = string.gsub(volumeCmd:read('*a'), '[\n\r]*', '')
-    volumeCmd:close()
-    --
-    local batteryCmd = io.popen("pmset -g batt | egrep '([0-9]+\\%)' -o | cut -f1 -d';'")
-    local battery = string.gsub(batteryCmd:read('*a'), '%%[\n\r]*', '')
-    batteryCmd:close()
-    --
-    local wifiCmd = io.popen("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | sed -e \"s/^ *SSID: //p\" -e d")
-    local wifi = string.gsub(wifiCmd:read('*a'), '[\n\r]*', '')
-    wifiCmd:close()
-    --
+    local volume = tostring(math.floor(hs.audiodevice.defaultOutputDevice():volume() + 0.5))
+    local battery = tostring(math.floor(hs.battery.percentage() + 0.5))
+    local wifi = hs.wifi.currentNetwork('en0')
+    if wifi == nil then
+        wifi = '(No Connection)'
+    end
 
     cv[1] = {
         type = 'text',
@@ -30,6 +23,7 @@ menuUpdater = hs.timer.doEvery(1, function()
         type = 'text',
         text = 'V '..volume..'%',
         textSize = 15.0,
+        textAlignment = 'left',
         frame = {x = 30, y = 0, w = 70, h = 20},
     }
 
@@ -37,18 +31,18 @@ menuUpdater = hs.timer.doEvery(1, function()
         type = 'text',
         text = 'B '..battery..'%',
         textSize = 15.0,
+        textAlignment = 'left',
         frame = {x = 100, y = 0, w = 70, h = 20},
     }
 
     cv[4] = {
         type = 'text',
         text = wifi,
-        textAlignment = 'right',
         textSize = 15.0,
+        textAlignment = 'right',
         frame = {x = 0, y = 0, w = max.w - 30, h = 20},
     }
-
 end)
-menuUpdater:start()
 
+menuUpdater:start()
 cv:show()
