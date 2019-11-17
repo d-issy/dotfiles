37644c9316812dc15b024e7be4e64790230b1843
d-issy <shepabashi@gmail.com>
2019-11-17 09:42:28 +0900

    update insert

--- .zsh/functions/self-insert.zsh
+++ .zsh/functions/self-insert.zsh
@@ -1,29 +1,35 @@
 self-insert() {
     zle .self-insert
     case $LBUFFER in
-        ' e'  ) LBUFFER='export '         ;;
-        ' gs' ) LBUFFER='git status'      ;;
-        ' ts' ) LBUFFER='tig status'      ;;
-        'c '  ) LBUFFER='cd '             ;;
-        'd '  ) LBUFFER='docker '         ;;
-        'dc ' ) LBUFFER='docker-compose ' ;;
-        'g '  ) LBUFFER='git '            ;;
-        'ga ' ) LBUFFER='git add '        ;;
-        'gb ' ) LBUFFER='git branch '     ;;
-        'gc ' ) LBUFFER='git checkout '   ;;
-        'gcl ') LBUFFER='git clone '      ;;
-        'gco ') LBUFFER='git commit '     ;;
-        'gf ' ) LBUFFER='git fetch '      ;;
-        'gpl ') LBUFFER='git pull '       ;;
-        'gps ') LBUFFER='git push '       ;;
-        'm '  ) LBUFFER='make '           ;;
-        'mc ' ) LBUFFER='make -C '        ;;
-        'mn ' ) LBUFFER='make -n '        ;;
-        'mnc ') LBUFFER='make -n -C '     ;;
-        'v '  ) LBUFFER='vim '            ;;
+        ' e'   ) LBUFFER='export '         ;;
+        ' gc'  ) LBUFFER='git clean -f'    ;;
+        ' gs'  ) LBUFFER='git status'      ;;
+        ' ts'  ) LBUFFER='tig status'      ;;
+        'c '   ) LBUFFER='cd '             ;;
+        'd '   ) LBUFFER='docker '         ;;
+        'dc '  ) LBUFFER='docker-compose ' ;;
+        'g '   ) LBUFFER='git '            ;;
+        'ga '  ) LBUFFER='git add '        ;;
+        'gb '  ) LBUFFER='git branch '     ;;
+        'gbd ' ) LBUFFER='git branch -d '  ;;
+        'gbdd ') LBUFFER='git branch -D '  ;;
+        'gc '  ) LBUFFER='git checkout '   ;;
+        'gcb ' ) LBUFFER='git checkout -b ';;
+        'gcl ' ) LBUFFER='git clone '      ;;
+        'gco ' ) LBUFFER='git commit '     ;;
+        'gf '  ) LBUFFER='git fetch '      ;;
+        'gpl ' ) LBUFFER='git pull '       ;;
+        'gps ' ) LBUFFER='git push '       ;;
+        'm '   ) LBUFFER='make '           ;;
+        'mc '  ) LBUFFER='make -C '        ;;
+        'mn '  ) LBUFFER='make -n '        ;;
+        'mnc ' ) LBUFFER='make -n -C '     ;;
+        'r '   ) LBUFFER='rm '             ;;
+        'rr '  ) LBUFFER='rm -rf '         ;;
+        'v '   ) LBUFFER='vim '            ;;
 
-        'git pull  ') LBUFFER='git pull origin ';;
-        'git push  ') LBUFFER='git push origin ';;
+        'git pull  ') LBUFFER="git pull origin $(git symbolic-ref --short HEAD)";;
+        'git push  ') LBUFFER="git push origin $(git symbolic-ref --short HEAD)";;
         'nis ') LBUFFER='npm install --save ';;
         'nid ') LBUFFER='npm install --save-dev ';;
 
