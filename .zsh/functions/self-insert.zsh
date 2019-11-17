aa5884541caa79c637c6fa4133034e5d92fed7c2
d-issy <daiki_ishizuka@voyagegroup.com>
2019-11-18 08:31:58 +0900

    fix

--- .zsh/functions/self-insert.zsh
+++ .zsh/functions/self-insert.zsh
@@ -28,8 +28,8 @@ self-insert() {
         'rr '  ) LBUFFER='rm -rf '         ;;
         'v '   ) LBUFFER='vim '            ;;
 
-        'git pull  ') LBUFFER="git pull origin $(git symbolic-ref --short HEAD)";;
-        'git push  ') LBUFFER="git push origin $(git symbolic-ref --short HEAD)";;
+        'git pull  ') LBUFFER="git pull origin $(git symbolic-ref --short HEAD 2> /dev/null)";;
+        'git push  ') LBUFFER="git push origin $(git symbolic-ref --short HEAD 2> /dev/null)";;
         'nis ') LBUFFER='npm install --save ';;
         'nid ') LBUFFER='npm install --save-dev ';;
 
