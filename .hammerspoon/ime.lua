1dd1430127b0f26dcd018df0bf7d51540156b19c
d-issy <shepabashi@gmail.com>
2020-06-19 22:32:13 +0900

    update ime

--- .hammerspoon/ime.lua
+++ .hammerspoon/ime.lua
@@ -42,7 +42,7 @@ eikanaEventTap = hs.eventtap.new({flagsChanged, keyDown}, function(event)
         if keyCode == Escape then
             hs.eventtap.keyStroke({}, Eisu, 0)
         elseif flags['ctrl'] and keyCode == LeftBracket then
-            hs.eventtap.keyStroke({}, Escape, 0)
+            hs.eventtap.keyStroke({}, Eisu, 0)
         end
         singleCmd = false
         return
