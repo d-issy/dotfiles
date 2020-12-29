1786b05b67ce0fd51e1833f53362af2ff3f12c4b
d-issy <12374694+d-issy@users.noreply.github.com>
2020-12-29 22:23:49 +0900

    add alias with abbr

---
+++ config/fish/conf.d/alias.fish
@@ -0,0 +1,28 @@
+## basic
+abbr -a -g c cd
+abbr -a -g q exit
+abbr -a -g rr rm -rf
+abbr -a -g v vim
+
+## docker
+abbr -a -g d  docker
+abbr -a -g dc docker-compose
+
+## git
+abbr -a -g ga  git add
+abbr -a -g gb  git branch
+abbr -a -g gbd git branch -D
+abbr -a -g gco git commit
+abbr -a -g gpl git pull
+abbr -a -g gps git push
+abbr -a -g gs  git status
+abbr -a -g gsw git switch
+
+abbr -a -g ts  tig status
+
+## make
+abbr -a -g m make
+
+## npm
+abbr -a -g nis npm install --save
+abbr -a -g nid npm install --save-dev
