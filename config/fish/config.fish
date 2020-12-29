77256bb900720b4a7c2f184ca7c15e2903f02b42
d-issy <12374694+d-issy@users.noreply.github.com>
2020-12-30 06:17:35 +0900

    add fisher

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,2 @@
 # starship theme setting
 starship init fish | source
-
-# fish config
-fish_vi_key_bindings
-set fish_greeting
-
-# set environment
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x TIGRC_USER $XDG_CONFIG_HOME/tig/config # not compatible for under 2.5.1
-set -x ZDOTDIR $XDG_CONFIG_HOME/zsh # not compatible
