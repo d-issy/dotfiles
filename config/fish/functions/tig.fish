980792b865b4910fd8804e5aae1fa4d9a0cc8614
d-issy <12374694+d-issy@users.noreply.github.com>
2020-12-29 20:23:11 +0900

    update comment

--- config/fish/functions/tig.fish
+++ config/fish/functions/tig.fish
@@ -1,3 +1,4 @@
 function tig
+	# not compatible for under v2.5.1
 	TIGRC_USER="$XDG_CONFIG_HOME/tig/config" command tig $argv
 end
--- config/fish/functions/tmux.fish
+++ config/fish/functions/tmux.fish
@@ -1,3 +1,4 @@
 function tmux
+	# not compatible for under v3.1
 	command tmux -f "$XDG_CONFIG_HOME/tmux/tmux.conf" $argv
 end
