68365598daa859ed14586d941adf7cfc2e913f11
d-issy <12374694+d-issy@users.noreply.github.com>
2020-12-30 00:17:37 +0900

    update

---
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,9 @@
+function fish_user_key_bindings
+  fish_vi_key_bindings insert
+
+  bind -M insert \ca beginning-of-line
+  bind -M insert \ce end-of-line
+  bind -M insert \ck kill-line
+  bind -M insert \cn down-or-search
+  bind -M insert \cp up-or-search
+end
