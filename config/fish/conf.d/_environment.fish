6dca2d8999459ff931a00077d95652e1a1872685
d-issy <12374694+d-issy@users.noreply.github.com>
2021-01-08 09:49:48 +0900

    add google-cloud-sdk settings

--- config/fish/conf.d/_environment.fish
+++ config/fish/conf.d/_environment.fish
@@ -16,6 +16,12 @@ set -g PATH
 ## basic
 set -gx PATH $PATH $HOME/.local/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
 
+## in macOS gcloud
+set -l DIR /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+if test -d $DIR
+  set -gx PATH $PATH $DIR
+end
+
 ## in WSL vscode
 set -l DIR /mnt/c/Users/$USER/AppData/Local/Programs/Microsoft\ VS\ Code/bin
 if test -d $DIR
