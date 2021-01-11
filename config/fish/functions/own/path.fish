2a7778f7ab3f9ba96cdc1b6be0b6188f073cb623
d-issy <12374694+d-issy@users.noreply.github.com>
2021-01-12 07:00:04 +0900

    refactor path.fish

--- config/fish/functions/own/path.fish
+++ config/fish/functions/own/path.fish
@@ -1,22 +1,16 @@
 function path
   if test (count $argv) = 0
-    for p in $PATH
-      echo $p
-    end
+    for p in $PATH; echo $p; end
   end
   for p in $argv
-    if test "$p" = "-f"
-      set FORCE 1
+    if test "$p" = "-f"; set FORCE 1; end
+    if not test -d $p; or contains $p $PATH
+      continue
     end
-    if not test -d $p
-        continue
-    end
-    if not contains $p $PATH
-      if test "$FORCE" = "1"
-        set -gx PATH $p $PATH
-      else
-        set -gx PATH $PATH $p
-      end
+    if test "$FORCE" = "1"
+      set -gx PATH $p $PATH
+    else
+      set -gx PATH $PATH $p
     end
   end
 end
