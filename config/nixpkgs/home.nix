6b68ca5123b911a7bba697d955aea7f78567ce74
d-issy <12374694+d-issy@users.noreply.github.com>
2021-08-29 16:41:27 +0900

    ...

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -43,19 +43,6 @@ in {
       ll = "ls -la";
       ts = "tig status";
     };
-    envExtra = ''
-if [ -e $HOME/.config ]; then
-	export XDG_CONFIG_HOME=$HOME/.config
-fi
-
-if [ -e $HOME/.config/zsh ]; then
-	export ZDOTDIR=$XDG_CONFIG_HOME/zsh
-fi
-
-if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
-	. $HOME/.nix-profile/etc/profile.d/nix.sh
-fi
-    '';
   };
 
   programs.starship = {
