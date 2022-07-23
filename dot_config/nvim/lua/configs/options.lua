68b258371adbcedc50d02f62d15feb781b246e26
d-issy <12374694+d-issy@users.noreply.github.com>
2022-07-24 02:51:03 +0900

    update options

--- dot_config/nvim/lua/configs/options.lua
+++ dot_config/nvim/lua/configs/options.lua
@@ -1,19 +1,23 @@
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cmdheight = 2
 vim.opt.termguicolors = true
 vim.opt.mouse = 'a'
 
-vim.opt.autoindent = true
-vim.opt.breakindent = true
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-vim.opt.expandtab = true
 
-vim.opt.smartindent = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.smartindent = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.list = true
 vim.opt.listchars = { tab = '>-', trail = '•', nbsp = '_' }
-
