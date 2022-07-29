7ad338a7dd2e02a80ac43eaba634ec9c0ae13f30
d-issy <12374694+d-issy@users.noreply.github.com>
2022-07-29 09:30:22 +0900

    add terminal commands

--- dot_config/nvim/lua/user/mappings.lua
+++ dot_config/nvim/lua/user/mappings.lua
@@ -10,13 +10,14 @@ map('n', 'gt', ':BufferLineCycleNext<CR>', opts)
 -- leader
 map('n', '<Leader>e', ':Neotree reveal float<CR>', opts)
 map('n', '<Leader>w', ':w<CR>', opts)
-map('n', '<Leader>t', ':ToggleTerm<CR>', opts)
 
 -- telescope
-local builtin = require 'telescope.builtin'
-local themes = require 'telescope.themes'
+local status_ok, _ = pcall(require, 'telescope')
+if status_ok then
+  local builtin = require 'telescope.builtin'
+  local themes = require 'telescope.themes'
 
-local no_preview_theme = themes.get_dropdown({ previewer = false })
-map('n', '<Leader>p', function() builtin.find_files(no_preview_theme) end, opts)
-map('n', '<Leader>P', builtin.commands, opts)
-
+  local no_preview_theme = themes.get_dropdown({ previewer = false })
+  map('n', '<Leader>p', function() builtin.find_files(no_preview_theme) end, opts)
+  map('n', '<Leader>P', builtin.commands, opts)
+end
--- dot_config/nvim/lua/user/terminal.lua
+++ dot_config/nvim/lua/user/terminal.lua
@@ -1,4 +1,9 @@
-require 'toggleterm'.setup {
+local status_ok, toggleterm = pcall(require, 'toggleterm')
+if not status_ok then
+  return
+end
+
+toggleterm.setup {
   size = 13,
   start_in_insert = true,
   shade_terminals = false,
@@ -14,3 +19,14 @@ end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- programs
+local Terminal = require 'toggleterm.terminal'.Terminal
+local tig_status = Terminal:new { cmd = 'tig status', direction = 'float' }
+
+-- mapping
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+map('n', '<Leader>t', ':ToggleTerm<CR>', opts)
+map('n', '<Leader>gs', function() tig_status:toggle() end, opts)
