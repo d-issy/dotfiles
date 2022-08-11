b97e9b0b7db66984d5ee65c21f3790432d170ae7
d-issy <12374694+d-issy@users.noreply.github.com>
2022-08-12 08:03:50 +0900

    previewer

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,11 @@ if not status_ok then
 end
 
 telescope.setup {
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    }
+  },
   extensions = {
     file_browser = {
       theme = 'dropdown',
@@ -25,11 +30,8 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 local builtin = require 'telescope.builtin'
-local themes = require 'telescope.themes'
 
-local no_preview_theme = themes.get_dropdown({ previewer = false })
-
-map('n', '<Leader>p', function() builtin.find_files(no_preview_theme) end, opts)
+map('n', '<Leader>p', function() builtin.find_files({ previewer = false }) end, opts)
 map('n', '<Leader>P', builtin.commands, opts)
 
 map('n', '<Leader>e', function()
