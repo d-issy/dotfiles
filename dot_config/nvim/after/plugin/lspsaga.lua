a465d1d81a5d9703bc333af74785c42c211657ce
d-issy <12374694+d-issy@users.noreply.github.com>
2022-08-26 17:06:16 +0900

    key mapping

--- dot_config/nvim/after/plugin/lspsaga.lua
+++ dot_config/nvim/after/plugin/lspsaga.lua
@@ -4,10 +4,3 @@ if not status_ok then
 end
 
 saga.init_lsp_saga {}
-
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', 'gd', '<cmd>Lspsaga lsp_finder<CR>', opts)
-vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', '<leader>d', '<cmd>Lspsaga show_line_diagnostics<CR>', opts)
-vim.keymap.set('n', '<leader>h', '<cmd>Lspsaga code_action<CR>', opts)
-vim.keymap.set('n', '<leader>r', '<cmd>Lspsaga rename<CR>', opts)
