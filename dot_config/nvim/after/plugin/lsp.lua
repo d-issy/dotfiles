c1b4442f917502f794e6fc483ae0923b34df22cc
issy <12374694+d-issy@users.noreply.github.com>
2022-10-30 11:48:03 +0900

    NeoVimの設定を改善 (#41)
    * fix deprecated
    
    cmp_nvim_lsp.update_capabilities is deprecated, use cmp_nvim_lsp.default_capabilities instead.
    
    * remove unused local variable
    
    * improve file search

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,4 @@
 -- lsp basics
-local map = vim.keymap.set
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
@@ -21,7 +20,7 @@ local on_attach = function(_, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require 'cmp_nvim_lsp'.update_capabilities(capabilities)
+capabilities = require 'cmp_nvim_lsp'.default_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 vim.diagnostic.config({
