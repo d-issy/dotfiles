11a02a13c8a528f88c604ac37a871fffabf934d9
d-issy <12374694+d-issy@users.noreply.github.com>
2022-12-08 20:25:05 +0900

    format lua scripts

--- home/dot_hammerspoon/helper.lua
+++ home/dot_hammerspoon/helper.lua
@@ -2,27 +2,27 @@ local obj = {}
 
 -- term check
 local terms = {
-    'com.apple.Terminal',
-    'com.googlecode.iterm2',
-    'com.jetbrains.intellij',
-    'com.microsoft.VSCode',
-    'com.parallels.desktop.console',
-    'io.alacritty',
+  'com.apple.Terminal',
+  'com.googlecode.iterm2',
+  'com.jetbrains.intellij',
+  'com.microsoft.VSCode',
+  'com.parallels.desktop.console',
+  'io.alacritty',
 }
 
 obj.isTerm = function(app)
-    if type(app) == 'string' then
-        app = hs.application.find(app)
-        if app == nil then
-            return false
-        end
+  if type(app) == 'string' then
+    app = hs.application.find(app)
+    if app == nil then
+      return false
     end
-    for i, t in ipairs(terms) do
-        if app:bundleID() == t then
-            return true
-        end
+  end
+  for i, t in ipairs(terms) do
+    if app:bundleID() == t then
+      return true
     end
-    return false
+  end
+  return false
 end
 
 return obj
--- home/dot_hammerspoon/keybind.lua
+++ home/dot_hammerspoon/keybind.lua
@@ -4,43 +4,43 @@ local helper = require('helper')
 keyCodes = hs.hotkey.modal.new(nil)
 
 function keyCodes:newMap(mods, key, fn)
-    self:bind(mods, key, fn, nil, fn)
+  self:bind(mods, key, fn, nil, fn)
 end
 
-keyCodes:newMap({'ctrl'}, 'W', function()
-    hs.eventtap.keyStroke({'alt'}, 'Delete', 0)
+keyCodes:newMap({ 'ctrl' }, 'W', function()
+  hs.eventtap.keyStroke({ 'alt' }, 'Delete', 0)
 end)
 
-keyCodes:newMap({'ctrl'}, 'N', function()
-    hs.eventtap.keyStroke({}, 'Down', 0)
+keyCodes:newMap({ 'ctrl' }, 'N', function()
+  hs.eventtap.keyStroke({}, 'Down', 0)
 end)
 
-keyCodes:newMap({'ctrl'}, 'P', function()
-    hs.eventtap.keyStroke({}, 'Up', 0)
+keyCodes:newMap({ 'ctrl' }, 'P', function()
+  hs.eventtap.keyStroke({}, 'Up', 0)
 end)
 
-keyCodes:newMap({'ctrl'}, 'H', function()
-    hs.eventtap.keyStroke({}, 'Left', 0)
+keyCodes:newMap({ 'ctrl' }, 'H', function()
+  hs.eventtap.keyStroke({}, 'Left', 0)
 end)
 
-keyCodes:newMap({'ctrl'}, 'L', function()
-    hs.eventtap.keyStroke({}, 'Right', 0)
+keyCodes:newMap({ 'ctrl' }, 'L', function()
+  hs.eventtap.keyStroke({}, 'Right', 0)
 end)
 
 -- hotkey toggle by application
 appWatcher = hs.window.filter.new(true)
 
 appWatcher:subscribe({
-    hs.window.filter.windowFocused,
+  hs.window.filter.windowFocused,
 }, function(win, app)
-    if helper.isTerm(app) then
-        keyCodes:exit()
-    else
-        keyCodes:enter()
-    end
+  if helper.isTerm(app) then
+    keyCodes:exit()
+  else
+    keyCodes:enter()
+  end
 end)
 
 --- reload
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'R', function()
-    hs.reload()
+hs.hotkey.bind({ 'cmd', 'alt', 'ctrl' }, 'R', function()
+  hs.reload()
 end)
