1a1fd68fcb240d764c102b38641e5a089af75adf
issy <12374694+d-issy@users.noreply.github.com>
2022-12-08 20:26:28 +0900

    Format lua scripts (#70)
    format lua scripts

--- home/dot_hammerspoon/ime.lua
+++ home/dot_hammerspoon/ime.lua
@@ -11,58 +11,58 @@ local flagsChanged = hs.eventtap.event.types.flagsChanged
 local keyDown      = hs.eventtap.event.types.keyDown
 
 -- eisu
-hs.hotkey.bind({'ctrl', 'shift'}, ';', function()
-    hs.eventtap.keyStroke({}, Eisu, 0)
+hs.hotkey.bind({ 'ctrl', 'shift' }, ';', function()
+  hs.eventtap.keyStroke({}, Eisu, 0)
 end)
 
 -- kana
-hs.hotkey.bind({'ctrl', 'shift'}, 'j', function()
-    hs.eventtap.keyStroke({}, Kana, 0)
+hs.hotkey.bind({ 'ctrl', 'shift' }, 'j', function()
+  hs.eventtap.keyStroke({}, Kana, 0)
 end)
 
 -- eikana
 --- variable
 local singleCmd = false
 --- event
-eikanaEventTap = hs.eventtap.new({flagsChanged, keyDown}, function(event)
-    local eventType = event:getType()
-    local keyCode = event:getKeyCode()
-    local flags = event:getFlags()
+eikanaEventTap = hs.eventtap.new({ flagsChanged, keyDown }, function(event)
+  local eventType = event:getType()
+  local keyCode = event:getKeyCode()
+  local flags = event:getFlags()
 
-    -- except app
-    local activeApp = hs.application.frontmostApplication()
-    if activeApp:bundleID() == 'com.vmware.fusion' or
-        activeApp:bundleID() == 'org.virtualbox.app.VirtualBox' then
-        singleCmd = false
-        return
-    end
+  -- except app
+  local activeApp = hs.application.frontmostApplication()
+  if activeApp:bundleID() == 'com.vmware.fusion' or
+      activeApp:bundleID() == 'org.virtualbox.app.VirtualBox' then
+    singleCmd = false
+    return
+  end
 
-    -- keyDown
-    if eventType == keyDown then
-        if keyCode == Escape then
-            hs.eventtap.keyStroke({}, Eisu, 0)
-        elseif flags['ctrl'] and keyCode == LeftBracket then
-            hs.eventtap.keyStroke({}, Escape, 0)
-            return true
-        end
-        singleCmd = false
-        return
+  -- keyDown
+  if eventType == keyDown then
+    if keyCode == Escape then
+      hs.eventtap.keyStroke({}, Eisu, 0)
+    elseif flags['ctrl'] and keyCode == LeftBracket then
+      hs.eventtap.keyStroke({}, Escape, 0)
+      return true
     end
+    singleCmd = false
+    return
+  end
 
-    -- cmdPress
-    if flags['cmd'] then
-        singleCmd = true
-        return
-    end
+  -- cmdPress
+  if flags['cmd'] then
+    singleCmd = true
+    return
+  end
 
-    -- cmdRelease
-    if singleCmd then
-        if keyCode == RightCmd then
-            hs.eventtap.keyStroke({}, Kana, 0)
-        elseif keyCode == LeftCmd then
-            hs.eventtap.keyStroke({}, Eisu, 0)
-        end
-        singleCmd = false
+  -- cmdRelease
+  if singleCmd then
+    if keyCode == RightCmd then
+      hs.eventtap.keyStroke({}, Kana, 0)
+    elseif keyCode == LeftCmd then
+      hs.eventtap.keyStroke({}, Eisu, 0)
     end
+    singleCmd = false
+  end
 
 end):start()
--- home/dot_hammerspoon/info.lua
+++ home/dot_hammerspoon/info.lua
@@ -1,8 +1,8 @@
 local helper = require('helper')
-hs.hotkey.bind({'alt'}, 'T', function()
-    hs.alert.closeAll()
-    hs.alert.show(os.date('%Y-%m-%d %H:%M'))
+hs.hotkey.bind({ 'alt' }, 'T', function()
+  hs.alert.closeAll()
+  hs.alert.show(os.date('%Y-%m-%d %H:%M'))
 
-    local app = hs.application.frontmostApplication()
-    hs.alert.show(app:bundleID())
+  local app = hs.application.frontmostApplication()
+  hs.alert.show(app:bundleID())
 end)
