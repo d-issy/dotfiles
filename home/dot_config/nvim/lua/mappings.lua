8048aa447f036b2de5e89581283f5de3b6753483
issy <12374694+d-issy@users.noreply.github.com>
2022-12-20 08:27:22 +0900

    neovim keymap settings (#80)

--- home/dot_config/nvim/lua/mappings.lua
+++ home/dot_config/nvim/lua/mappings.lua
@@ -1,5 +1,13 @@
+vim.g.mapleader = ' '
 local opts = { noremap = true, silent = true }
 
-vim.g.mapleader = ' '
-
+vim.keymap.set('n', 'x', '"_x')
 vim.keymap.set('n', '<Leader>w', ':w<CR>', opts)
+
+vim.keymap.set('n', '<Leader>sh', ':split<CR>', opts)
+vim.keymap.set('n', '<Leader>sv', ':vsplit<CR>', opts)
+vim.keymap.set('n', '<Leader>x', ':close<CR>', opts)
+
+vim.keymap.set('n', '<Leader>tc', ':tabnew<CR>', opts)
+vim.keymap.set('n', '<Leader>tn', ':tabnext<CR>', opts)
+vim.keymap.set('n', '<Leader>tp', ':tabprevious<CR>', opts)
