334ec29a02775c077ca9076a91d10019b0cb52f2
d-issy <12374694+d-issy@users.noreply.github.com>
2023-01-04 22:42:04 +0900

    fix helper

--- home/.chezmoitemplates/hammerspoon/helper.lua
+++ home/.chezmoitemplates/hammerspoon/helper.lua
@@ -1,35 +1,37 @@
-Helper = {}
+Helper = (function()
+  local M = {}
+  M.isTargetApp = function(targetApps, app)
+    if type(app) == 'string' then
+      app = hs.application.find(app)
+    elseif app == nil then
+      app = hs.application.frontmostApplication()
+    end
 
-Helper.isTargetApp = function(targetApps, app)
-  if type(app) == 'string' then
-    app = hs.application.find(app)
-  elseif app == nil then
-    app = hs.application.frontmostApplication()
+    if app == nil then return false end
+
+    for _, target in ipairs(targetApps) do
+      if app:bundleID() == target then return true end
+    end
+    return false
   end
 
-  if app == nil then return false end
-
-  for _, target in ipairs(targetApps) do
-    if app:bundleID() == target then return true end
+  M.isTerminalApp = function(app)
+    return Helper.isTargetApp({
+      'com.apple.Terminal',
+      'com.googlecode.iterm2',
+      'com.jetbrains.intellij',
+      'com.microsoft.VSCode',
+      'com.github.wez.wezterm',
+      'org.alacritty',
+    }, app)
   end
-  return false
-end
 
-Helper.isTerminalApp = function(app)
-  return Helper.isTargetApp({
-    'com.apple.Terminal',
-    'com.googlecode.iterm2',
-    'com.jetbrains.intellij',
-    'com.microsoft.VSCode',
-    'com.github.wez.wezterm',
-    'org.alacritty',
-  }, app)
-end
-
-Helper.isVirtualMachineApp = function(app)
-  return Helper.isTargetApp({
-    'com.parallels.desktop.console',
-    'com.vmware.fusion',
-    'org.virtualbox.app.VirtualBox',
-  }, app)
-end
+  M.isVirtualMachineApp = function(app)
+    return Helper.isTargetApp({
+      'com.parallels.desktop.console',
+      'com.vmware.fusion',
+      'org.virtualbox.app.VirtualBox',
+    }, app)
+  end
+  return M
+end)()
