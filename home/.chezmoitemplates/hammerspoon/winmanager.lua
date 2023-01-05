87fde814c62fdfed6df90deef0fefeb2cd5fabf5
d-issy <12374694+d-issy@users.noreply.github.com>
2023-01-05 23:02:21 +0900

    refactor

--- home/.chezmoitemplates/hammerspoon/winmanager.lua
+++ home/.chezmoitemplates/hammerspoon/winmanager.lua
@@ -1,21 +1,22 @@
 WinManager = (function()
-  local margin = 10
-  local diff = 30
+  local MARGIN = 10
+  local DIFF = 30
 
   hs.window.animationDuration = 0
-  hs.grid.setMargins({ x = margin, y = margin })
+  hs.grid.setMargins({ x = MARGIN, y = MARGIN })
 
   local M = { _ = {} }
   local currentWindow = function()
     return hs.window.frontmostWindow()
   end
 
-  local moveGrid = function(w, h, x, y, win)
-    hs.grid.setGrid(string.format('%dx%d', w, h))
+  local moveGrid = function(win, rect)
+    rect = rect or {}
+    hs.grid.setGrid(string.format('%dx%d', rect.w or 1, rect.h or 1))
     if win == nil then
       win = currentWindow()
     end
-    hs.grid.set(win, { x = x - 1, y = y - 1, w = 1, h = 1 })
+    hs.grid.set(win, { x = (rect.x or 1) - 1, y = (rect.y or 1) - 1, w = 1, h = 1 })
   end
 
   local findApplication = function(app)
@@ -44,18 +45,18 @@ WinManager = (function()
     win:setTopLeft({
       x = Helper.clamp(
         f.x + (pos.x or 0),
-        margin,
-        screen.w - f.w - margin
+        MARGIN,
+        screen.w - f.w - MARGIN
       ),
       y = Helper.clamp(
         f.y + (pos.y or 0),
-        margin + screen.y,
-        screen.h - f.h - margin + screen.y
+        MARGIN + screen.y,
+        screen.h - f.h - MARGIN + screen.y
       )
     })
   end
 
-  M._._arrangeMode = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
+  M._.arangeMode = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
     local quit = true
     local key = hs.keycodes.map[event:getKeyCode()]
     local win = currentWindow()
@@ -63,54 +64,54 @@ WinManager = (function()
     local fn = ({
       a = function() M.moveFull() end,
       h = function()
-        setWindowPositionDiff(win, { x = -diff })
+        setWindowPositionDiff(win, { x = -DIFF })
         quit = false
       end,
       j = function()
-        setWindowPositionDiff(win, { y = diff })
+        setWindowPositionDiff(win, { y = DIFF })
         quit = false
       end,
       k = function()
-        setWindowPositionDiff(win, { y = -diff })
+        setWindowPositionDiff(win, { y = -DIFF })
         quit = false
       end,
       l = function()
-        setWindowPositionDiff(win, { x = diff })
+        setWindowPositionDiff(win, { x = DIFF })
         quit = false
       end,
       H = function()
-        setWindowPositionDiff(win, { x = -diff })
+        setWindowPositionDiff(win, { x = -DIFF })
         quit = false
       end,
       J = function()
-        setWindowPositionDiff(win, { y = diff })
+        setWindowPositionDiff(win, { y = DIFF })
         quit = false
       end,
       K = function()
-        setWindowPositionDiff(win, { y = -diff })
+        setWindowPositionDiff(win, { y = -DIFF })
         quit = false
       end,
       L = function()
-        setWindowPositionDiff(win, { x = diff })
+        setWindowPositionDiff(win, { x = DIFF })
         quit = false
       end,
     })[key]
     if fn then fn() end
 
-    if quit then M._._arrangeMode:stop() end
+    if quit then M._.arangeMode:stop() end
     return true
   end)
 
   M.arrangeMode = function()
-    if not M._._arrangeMode:isEnabled() then
-      M._._arrangeMode:start()
+    if not M._.arangeMode:isEnabled() then
+      M._.arangeMode:start()
       hs.alert.show('ArrangeMode')
     end
   end
 
-  M.moveLeft = function(win) moveGrid(2, 1, 1, 1, win) end
-  M.moveRight = function(win) moveGrid(2, 1, 2, 1, win) end
-  M.moveFull = function(win) moveGrid(1, 1, 1, 1, win) end
+  M.moveLeft = function(win) moveGrid(win, { w = 2, x = 1 }) end
+  M.moveRight = function(win) moveGrid(win, { w = 2, x = 2 }) end
+  M.moveFull = function(win) moveGrid(win) end
   M.toggleZoom = function() currentWindow():toggleZoom() end
   M.toggleFullScreen = function() currentWindow():toggleFullScreen() end
 
