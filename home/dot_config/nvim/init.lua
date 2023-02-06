6da4e897c00a31d99187574a641bfa725ec18d57
d-issy <12374694+d-issy@users.noreply.github.com>
2023-02-07 08:27:08 +0900

    colorscheme

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -53,3 +53,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require 'lazy'.setup('plugins')
+
+require 'tokyonight'.load()
---
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return{
+
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+  },
+}
