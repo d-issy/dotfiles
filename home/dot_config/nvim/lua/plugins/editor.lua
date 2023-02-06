1ac8a843146eda02160eb9eb46d6bc8423b2ddb9
d-issy <12374694+d-issy@users.noreply.github.com>
2023-02-07 08:49:16 +0900

    single quotes

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -1,23 +1,23 @@
 return {
   -- file explorer
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    cmd = "Neotree", branch='v2.x',
+    'nvim-neo-tree/neo-tree.nvim',
+    cmd = 'Neotree', branch='v2.x',
     keys = {
       {
-        "<leader>e",
+        '<leader>e',
         function()
-          require("neo-tree.command").execute({ toggle = true })
+          require('neo-tree.command').execute({ toggle = true })
         end,
-        desc = "Explorer NeoTree (root dir)"
+        desc = 'Explorer NeoTree (root dir)'
       }
     },
     init = function()
       vim.g.neo_tree_remove_legacy_commands = 1
       if vim.fn.argc() == 1 then
         local stat = vim.loop.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == "directory" then
-          require("neo-tree")
+        if stat and stat.type == 'directory' then
+          require('neo-tree')
         end
       end
     end,
@@ -28,7 +28,7 @@ return {
       },
       window = {
         mappings = {
-          ["<space>"] = "none",
+          ['<space>'] = 'none',
         },
       },
     },
@@ -50,11 +50,11 @@ return {
 
   -- easily jump to any location and enhanced f/t motions for Leap
   {
-    "ggandor/leap.nvim",
-    event = "VeryLazy",
-    dependencies = { { "ggandor/flit.nvim", opts = { labeled_modes = "nv" } } },
+    'ggandor/leap.nvim',
+    event = 'VeryLazy',
+    dependencies = { { 'ggandor/flit.nvim', opts = { labeled_modes = 'nv' } } },
     config = function(_, opts)
-      local leap = require("leap")
+      local leap = require('leap')
       for k, v in pairs(opts) do
         leap.opts[k] = v
       end
@@ -64,48 +64,48 @@ return {
 
     -- which-key
   {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
     opts = {
       plugins = { spelling = true },
     },
     config = function(_, opts)
-      local wk = require("which-key")
+      local wk = require('which-key')
       wk.setup(opts)
       wk.register({
-        mode = { "n", "v" },
-        ["g"] = { name = "+goto" },
-        ["gz"] = { name = "+surround" },
-        ["]"] = { name = "+next" },
-        ["["] = { name = "+prev" },
-        ["<leader><tab>"] = { name = "+tabs" },
-        ["<leader>b"] = { name = "+buffer" },
-        ["<leader>c"] = { name = "+code" },
-        ["<leader>f"] = { name = "+file/find" },
-        ["<leader>g"] = { name = "+git" },
-        ["<leader>gh"] = { name = "+hunks" },
-        ["<leader>q"] = { name = "+quit/session" },
-        ["<leader>s"] = { name = "+search" },
-        ["<leader>sn"] = { name = "+noice" },
-        ["<leader>u"] = { name = "+ui" },
-        ["<leader>w"] = { name = "+windows" },
-        ["<leader>x"] = { name = "+diagnostics/quickfix" },
+        mode = { 'n', 'v' },
+        ['g'] = { name = '+goto' },
+        ['gz'] = { name = '+surround' },
+        [']'] = { name = '+next' },
+        ['['] = { name = '+prev' },
+        ['<leader><tab>'] = { name = '+tabs' },
+        ['<leader>b'] = { name = '+buffer' },
+        ['<leader>c'] = { name = '+code' },
+        ['<leader>f'] = { name = '+file/find' },
+        ['<leader>g'] = { name = '+git' },
+        ['<leader>gh'] = { name = '+hunks' },
+        ['<leader>q'] = { name = '+quit/session' },
+        ['<leader>s'] = { name = '+search' },
+        ['<leader>sn'] = { name = '+noice' },
+        ['<leader>u'] = { name = '+ui' },
+        ['<leader>w'] = { name = '+windows' },
+        ['<leader>x'] = { name = '+diagnostics/quickfix' },
       })
     end
   },
 
     -- git signs
   {
-    "lewis6991/gitsigns.nvim",
-    event = "BufReadPre",
+    'lewis6991/gitsigns.nvim',
+    event = 'BufReadPre',
     opts = {
       signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "契" },
-        topdelete = { text = "契" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
+        add = { text = '▎' },
+        change = { text = '▎' },
+        delete = { text = '契' },
+        topdelete = { text = '契' },
+        changedelete = { text = '▎' },
+        untracked = { text = '▎' },
       },
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
@@ -115,30 +115,30 @@ return {
         end
 
         -- stylua: ignore start
-        map("n", "]h", gs.next_hunk, "Next Hunk")
-        map("n", "[h", gs.prev_hunk, "Prev Hunk")
-        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-        map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-        map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-        map("n", "<leader>ghd", gs.diffthis, "Diff This")
-        map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+        map('n', ']h', gs.next_hunk, 'Next Hunk')
+        map('n', '[h', gs.prev_hunk, 'Prev Hunk')
+        map({ 'n', 'v' }, '<leader>ghs', ':Gitsigns stage_hunk<CR>', 'Stage Hunk')
+        map({ 'n', 'v' }, '<leader>ghr', ':Gitsigns reset_hunk<CR>', 'Reset Hunk')
+        map('n', '<leader>ghS', gs.stage_buffer, 'Stage Buffer')
+        map('n', '<leader>ghu', gs.undo_stage_hunk, 'Undo Stage Hunk')
+        map('n', '<leader>ghR', gs.reset_buffer, 'Reset Buffer')
+        map('n', '<leader>ghp', gs.preview_hunk, 'Preview Hunk')
+        map('n', '<leader>ghb', function() gs.blame_line({ full = true }) end, 'Blame Line')
+        map('n', '<leader>ghd', gs.diffthis, 'Diff This')
+        map('n', '<leader>ghD', function() gs.diffthis('~') end, 'Diff This ~')
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'GitSigns Select Hunk')
       end,
     }
   },
 
   -- better diagnostics list and others
   {
-    "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
+    'folke/trouble.nvim',
+    cmd = { 'TroubleToggle', 'Trouble' },
     opts = { use_diagnostic_signs = true },
     keys = {
-      { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+      { '<leader>xx', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Document Diagnostics (Trouble)' },
+      { '<leader>xX', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnostics (Trouble)' },
     }
   },
 
