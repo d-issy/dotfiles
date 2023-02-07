faf8731f42d724bf4096ed67b25be7c85ff00f67
d-issy <12374694+d-issy@users.noreply.github.com>
2023-02-07 21:49:40 +0900

    add lsp settings

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -10,13 +10,50 @@ return {
     opts = {
       ensure_installed = { 'sumneko_lua' },
       servers = {
+        sumneko_lua={
+          settings = { Lua = {
+            diagnostics = {globals={'vim', 'hs'}},
+            workspace = {
+              library = vim.api.nvim_get_runtime_file('', true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false }
+          } }
+        },
+      },
+      pyright = {
+        settings = {python = {analysis={
+          autoImportCompletions=true,
+          autoSearchPaths=true,
+          diagnosticMode = 'workspace',
+          useLibraryCodeForTypes = true,
+        }}}
       }
     },
     config = function(_, opts)
       require('mason').setup {}
-      require('mason-lspconfig').setup {
-        ensure_installed = opts.ensure_installed
+      require('mason-lspconfig').setup { ensure_installed = opts.ensure_installed or {} }
+
+      require('mason-lspconfig').setup_handlers {
+        function(name)
+          print(name)
+          print(vim.inspect(opts.servers[name] or {}))
+          local lsp_opts =  opts.servers[name] or {}
+          require('lspconfig')[name].setup(lsp_opts)
+        end
       }
+      -- require('lspconfig')['sumneko_lua'].setup{
+      --   settings = {
+      --     Lua = {
+      --       diagnostics = {globals={'vim', 'hs'}},
+      --       workspace = {
+      --         library = vim.api.nvim_get_runtime_file('', true),
+      --         checkThirdParty = false,
+      --       },
+      --       telemetry = { enable = false }
+      --     }
+      --   }
+      -- }
     end
   }
 }
