6200dc8e60db92a1acc9378e702f35f000c13ef2
d-issy <12374694+d-issy@users.noreply.github.com>
2023-02-08 00:04:57 +0900

    ...

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -5,10 +5,10 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.cmdheight = 2
 vim.opt.termguicolors = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 vim.opt.wrap = false
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 vim.opt.autoindent = true
 vim.opt.smartindent = true
@@ -19,39 +19,39 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
 
-vim.opt.path:append { '**' }
+vim.opt.path:append({ "**" })
 
 vim.opt.list = true
-vim.opt.listchars = { tab = '>-', trail = '•', nbsp = '_' }
+vim.opt.listchars = { tab = "  ", trail = "•", nbsp = "_" }
 
 -- keys
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set('n', 'x', '"_x')
-vim.keymap.set('n', '<Leader>w', ':w<CR>', opts)
+vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("n", "<Leader>w", ":w<CR>", opts)
 
-vim.keymap.set('n', '<Leader>sh', ':split<CR>', opts)
-vim.keymap.set('n', '<Leader>sv', ':vsplit<CR>', opts)
-vim.keymap.set('n', '<Leader>x', ':close<CR>', opts)
+vim.keymap.set("n", "<Leader>sh", ":split<CR>", opts)
+vim.keymap.set("n", "<Leader>sv", ":vsplit<CR>", opts)
+vim.keymap.set("n", "<Leader>x", ":close<CR>", opts)
 
-vim.keymap.set('n', '<Leader>tc', ':tabnew<CR>', opts)
-vim.keymap.set('n', '<Leader>tn', ':tabnext<CR>', opts)
-vim.keymap.set('n', '<Leader>tp', ':tabprevious<CR>', opts)
+vim.keymap.set("n", "<Leader>tc", ":tabnew<CR>", opts)
+vim.keymap.set("n", "<Leader>tn", ":tabnext<CR>", opts)
+vim.keymap.set("n", "<Leader>tp", ":tabprevious<CR>", opts)
 
 -- plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-require 'lazy'.setup('plugins')
+require("lazy").setup("plugins")
 
-vim.cmd 'colorscheme edge'
+vim.cmd("colorscheme edge")
--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -17,6 +17,103 @@ return {
 		end,
 	},
 
+	-- indent guides for Neovim
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufReadPost",
+		opts = {
+			char = "▏",
+			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+			show_trailing_blankline_indent = false,
+			show_current_context = false,
+		},
+	},
+
+	-- noicer ui
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			cmdline = {
+				view = "cmdline",
+				format = {
+					cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+					search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+					search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+					filter = { pattern = "^:%s*!", icon = "", lang = "bash" },
+					lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+					help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+				},
+			},
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = false,
+				long_message_to_split = true,
+			},
+		},
+		keys = {
+			{
+				"<S-Enter>",
+				function()
+					require("noice").redirect(vim.fn.getcmdline())
+				end,
+				mode = "c",
+				desc = "Redirect Cmdline",
+			},
+			{
+				"<leader>snl",
+				function()
+					require("noice").cmd("last")
+				end,
+				desc = "Noice Last Message",
+			},
+			{
+				"<leader>snh",
+				function()
+					require("noice").cmd("history")
+				end,
+				desc = "Noice History",
+			},
+			{
+				"<leader>sna",
+				function()
+					require("noice").cmd("all")
+				end,
+				desc = "Noice All",
+			},
+			{
+				"<c-f>",
+				function()
+					if not require("noice.lsp").scroll(4) then
+						return "<c-f>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll forward",
+				mode = { "i", "n", "s" },
+			},
+			{
+				"<c-b>",
+				function()
+					if not require("noice.lsp").scroll(-4) then
+						return "<c-b>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll backward",
+				mode = { "i", "n", "s" },
+			},
+		},
+	},
+
 	-- ui parts
 	{ "nvim-lua/plenary.nvim", lazy = true },
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
