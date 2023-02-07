80765e6fd6f0e6e467b633706f6d56da8ac9c07e
d-issy <12374694+d-issy@users.noreply.github.com>
2023-02-07 09:17:43 +0900

    lsp

---
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
+    },
+    opts = {
+      ensure_installed = { 'sumneko_lua' }
+      servers = {
+      }
+    },
+    config = function(_, opts)
+      require('mason').setup {}
+      require('mason-lspconfig').setup {
+        ensure_installed = opts.ensure_installed
+      }
+    end
+  }
+}
