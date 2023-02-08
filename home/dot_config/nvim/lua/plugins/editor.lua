79fec8f9f8001ea54f9ad083d1a5b0d890c15222
d-issy <12374694+d-issy@users.noreply.github.com>
2023-02-08 21:00:49 +0900

    remove signs

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -100,14 +100,6 @@ return {
     'lewis6991/gitsigns.nvim',
     event = 'BufReadPre',
     opts = {
-      signs = {
-        add = { text = '▎' },
-        change = { text = '▎' },
-        delete = { text = '契' },
-        topdelete = { text = '契' },
-        changedelete = { text = '▎' },
-        untracked = { text = '▎' },
-      },
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
 
