cd5d51a4fa0cc0f53a9435f4a93ff098f2e96a82
d-issy <12374694+d-issy@users.noreply.github.com>
2024-05-01 10:54:53 +0900

    Set Theme to Catppuccin

---
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+-- comment
+return {
+  "catppuccin",
+  opts = {
+    flavour = "auto",
+    background = {
+      light = "latte",
+      dark = "macchiato",
+    },
+    transparent_background = true,
+    no_italic = false,
+    styles = {
+      comments = { "italic" },
+    },
+  },
+}
