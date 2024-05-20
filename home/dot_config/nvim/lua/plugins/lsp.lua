412dfb615dede067e8e797d98418c6054b213be4
d-issy <12374694+d-issy@users.noreply.github.com>
2024-05-20 10:15:06 +0900

    diagnostic border

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,11 @@ return {
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
+    -- ui settings
+    if vim.fn.has "nvim-0.5.0" == 1 then
+      vim.diagnostic.config { float = { border = "rounded" } }
+    end
+
     -- disable signature help keymap
     keys[#keys + 1] = { "<c-k>", false, mode = "i" }
   end,
