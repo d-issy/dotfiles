c1de6fe01844f455f3b488e21919f8c11b333a0e
d-issy <12374694+d-issy@users.noreply.github.com>
2024-05-25 23:55:55 +0900

    launch lazygit for tmux

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,12 @@
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr><esc>", { silent = true, desc = "Write" })
-vim.keymap.set(
-  "n",
-  "<leader>gg",
-  function() require("lazyvim.util").terminal.open "lazygit" end,
-  { silent = true, desc = "LazyGit" }
-)
+vim.keymap.set("n", "<leader>gg", function()
+  -- tmux session
+  if vim.fn.getenv "TMUX" then
+    vim.fn.system { "tmux", "popup", "-d", "#{pane_current_path}", "-h", "95%", "-w", "95%", "-E", "lazygit" }
+  else
+    require("lazyvim.util").terminal.open "lazygit"
+  end
+end, { silent = true, desc = "LazyGit" })
 
 -- disable terminal switching
 vim.keymap.del("t", "<C-h>")
