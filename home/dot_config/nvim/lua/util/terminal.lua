e1a020e9f397960b890bec307f67b57eadd6b164
d-issy <12374694+d-issy@users.noreply.github.com>
2024-05-26 01:25:06 +0900

    fix

--- home/dot_config/nvim/lua/util/terminal.lua
+++ home/dot_config/nvim/lua/util/terminal.lua
@@ -2,14 +2,16 @@ local array = require "util.array"
 
 M = {}
 
----Open a terminal window with the given command
+---@return boolean
+function M.is_tmux_enabled() return vim.fn.getenv "TMUX" ~= vim.NIL end
+
 ---@param cmd string[]|string
 function M.open(cmd)
   if type(cmd) == "string" then
     cmd = { cmd }
   end
 
-  if vim.fn.getenv "TMUX" then
+  if M.is_tmux_enabled() then
     local tmux_popup = { "tmux", "popup", "-d", "#{pane_current_path}", "-h", "95%", "-w", "95%", "-E" }
     vim.fn.system(array.concat(tmux_popup, cmd))
   else
