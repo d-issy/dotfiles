052a3dffd0723180ed305a090711078ed93a34ed
d-issy <12374694+d-issy@users.noreply.github.com>
2024-05-26 19:00:47 +0900

    add --force for chezmoi apply

--- home/dot_config/nvim/lua/util/chezmoi.lua
+++ home/dot_config/nvim/lua/util/chezmoi.lua
@@ -7,7 +7,7 @@ function M.is_enabled() return vim.fn.executable "chezmoi" == 1 end
 
 function M.apply()
   if M.is_enabled() then
-    local out = vim.fn.trim(vim.fn.system { "chezmoi", "apply", "--verbose" })
+    local out = vim.fn.trim(vim.fn.system { "chezmoi", "apply", "--verbose", "--force" })
     if out ~= "" then
       vim.notify("chezmoi applyed\n" .. out)
     end
