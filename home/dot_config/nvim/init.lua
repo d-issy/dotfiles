095e1a40f0aee847c8f0da353f71de715eccd615
d-issy <12374694+d-issy@users.noreply.github.com>
2024-05-26 14:07:34 +0900

    enable extra.coding.mini-surround

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -29,6 +29,7 @@ require("lazy").setup {
 
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
