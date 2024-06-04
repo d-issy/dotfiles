f90421a0d1c4b9ee2ceb213d546d274d50c04421
issy <12374694+d-issy@users.noreply.github.com>
2024-06-05 01:05:04 +0900

    Use wezterm for wsl (#159)
    wezterm config

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,5 +1,24 @@
 local wezterm = require "wezterm"
 
+local config = {}
+
+config.color_scheme = "Catppuccin Macchiato"
+config.font = wezterm.font_with_fallback { "Hack Nerd Font Mono", "Hiragino Mincho Pro W3" }
+config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
+config.use_ime = true
+config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
+config.keys = {
+  { key = "q", mods = "CTRL", action = wezterm.action.SendKey { key = "q", mods = "CTRL" } },
+  { key = "u", mods = "CMD", action = wezterm.action.EmitEvent "toggle-opacity" },
+}
+
+local wsl_domains = wezterm.default_wsl_domains()
+if #wsl_domains > 0 then
+  config.default_domain = wsl_domains[1].name
+end
+
 wezterm.on("toggle-opacity", function(window, pane)
   local overrides = window:get_config_overrides() or {}
   if not overrides.window_background_opacity then
@@ -17,24 +36,4 @@ wezterm.on("toggle-opacity", function(window, pane)
   window:set_config_overrides(overrides)
 end)
 
-return {
-  color_scheme = "Catppuccin Macchiato",
-  font = wezterm.font_with_fallback {
-    "Hack Nerd Font Mono",
-    "Hiragino Mincho Pro W3",
-  },
-
-  font_size = 14,
-
-  hide_tab_bar_if_only_one_tab = true,
-  adjust_window_size_when_changing_font_size = false,
-
-  -- key bindings
-  use_ime = true,
-  macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
-  keys = {
-    -- for tmux key binding for use-ime
-    { key = "q", mods = "CTRL", action = wezterm.action.SendKey { key = "q", mods = "CTRL" } },
-    { key = "u", mods = "CMD", action = wezterm.action.EmitEvent "toggle-opacity" },
-  },
-}
+return config
