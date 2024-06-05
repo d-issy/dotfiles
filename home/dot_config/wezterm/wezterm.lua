d6530db651e9eea0289b86d49c3c8d2b2bf2db6e
issy <12374694+d-issy@users.noreply.github.com>
2024-06-06 07:56:42 +0900

    font settings (#160)
    * font settings
    
    * update

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -2,8 +2,21 @@ local wezterm = require "wezterm"
 
 local config = {}
 
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
 config.color_scheme = "Catppuccin Macchiato"
-config.font = wezterm.font_with_fallback { "Hack Nerd Font Mono", "Hiragino Mincho Pro W3" }
+config.font = wezterm.font_with_fallback {
+  "0xProto",
+  "UDEV Gothic 35",
+  "Cica",
+  "Hack Nerd Font Mono",
+  "YuGothic",
+}
+config.warn_about_missing_glyphs = false
+config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" } -- ligatures
+
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
