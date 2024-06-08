6728876033bb411ff14b3dbf45d08b1c71e9fa02
issy <12374694+d-issy@users.noreply.github.com>
2024-06-08 22:26:48 +0900

    neovim UI and refresh git (#168)

--- home/dot_config/nvim/lua/util/git.lua
+++ home/dot_config/nvim/lua/util/git.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.refresh()
   if package.loaded["neo-tree.sources.git_status"] then
     require("neo-tree.sources.git_status").refresh()
+    vim.cmd "checktime"
   end
 end
 
