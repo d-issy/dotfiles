b6bb45b46bc3a14ccb77fad6ee1af00dc71de048
d-issy <12374694+d-issy@users.noreply.github.com>
2024-06-16 04:20:35 +0900

    uis

---
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    event = { "BufReadPost" },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        ---@diagnostic disable-next-line: unused-local
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match "error" and " " or " "
+          return " " .. icon .. count
+        end,
+        always_show_bufferline = false,
+      },
+    },
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+      { "<leader>bl", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      { "<leader>bh", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+      { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+    end,
+  },
+  {
+    "Bekaboo/dropbar.nvim",
+    event = { "BufReadPost" },
+    config = true,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    event = { "BufReadPost" },
+    config = true,
+  },
+}
