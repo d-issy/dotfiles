26b117cfeefbf1bc1b6553a12f082d1799fa794a
d-issy <12374694+d-issy@users.noreply.github.com>
2024-06-16 08:17:41 +0900

    actions

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -1,20 +1,19 @@
 local keymap = require "util.keymap"
 
-keymap.set("<leader>w", "<cmd>w<cr><esc>", "Write")
+keymap.set("<leader>w", "<cmd>w<cr>", "Write")
+keymap.set("<leader>qq", "<cmd>qa<cr>", "Quit and Save All")
 keymap.set("<leader>md", "<cmd>delmarks! | delmarks A-Z0-9<cr>", "Delete All Marks")
 
--- local lazygit = function() require("util.lazygit").open { cwd = require("lazyvim.util").root.git() } end
--- keymap.set("<leader>gg", lazygit, "Lazygit (Root Dir)")
--- keymap.set("<leader>gG", function() require("util.lazygit").open() end, "Lazygit (cwd)")
--- keymap.set("<leader>gf", function() require("util.lazygit").file_history() end, "Current File History")
--- keymap.set("<leader>gl", function() require("util.lazygit").commit_log() end, "Commit Log")
-
--- local term = function() require("util.terminal").open(nil, { cwd = require("lazyvim.util").root() }) end
--- keymap.set("<leader>ft", term, "Terminal (Root Dir)")
--- keymap.set("<leader>fT", function() require("util.terminal").open() end, "Terminal (cwd)")
-
--- -- disable terminal switching
--- keymap.delete("<C-h>", "t")
--- keymap.delete("<C-j>", "t")
--- keymap.delete("<C-k>", "t")
--- keymap.delete("<C-l>", "t")
+local lazygit = function()
+  require("util.lazygit").open { cwd = require("lazyvim.util").root.git() }
+end
+keymap.set("<leader>gg", lazygit, "Lazygit (Root Dir)")
+keymap.set("<leader>gG", function()
+  require("util.lazygit").open()
+end, "Lazygit (cwd)")
+keymap.set("<leader>gf", function()
+  require("util.lazygit").file_history()
+end, "Current File History")
+keymap.set("<leader>gl", function()
+  require("util.lazygit").commit_log()
+end, "Commit Log")
