74abb2fbedb2993f89f3204a79356eb4b1c1f92b
d-issy <12374694+d-issy@users.noreply.github.com>
2024-06-16 04:20:24 +0900

    formatters

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -4,7 +4,20 @@ return {
   event = { "BufReadPost" },
   opts = {
     formatters_by_ft = {
-      lua = { "stylua" },
+      ['lua'] = { 'stylua' },
+      ["python"] = { "black", "isort" },
+      ["go"] = { "goimports", "gofumpt" },
+
+      ["javascript"] = { { "prettierd", "prettier" } },
+      ["javascriptreact"] = { { "prettierd", "prettier" } },
+      ["typescript"] = { { "prettierd", "prettier" } },
+      ["typescriptreact"] = { { "prettierd", "prettier" } },
+      ["markdown"] = { { "prettierd", "prettier" }, "markdownlint", "markdown-doc" },
+      ["markdown.mdx"] = { { "prettierd", "prettier" }, "markdownlint", "markdown-doc" },
+
+      ["terraform"] = { "terraform_fmt" },
+      ["terraform-vars"] = { "terraform_fmt" },
+      ["tf"] = { "terraform_fmt" },
     },
     format_on_save = {
       timeout_ms = 500,
