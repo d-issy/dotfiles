a487d52adec55d4bc71535e77803d7f118fd4547
d-issy <12374694+d-issy@users.noreply.github.com>
2024-06-16 23:39:56 +0900

    indent

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -18,4 +18,34 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.indentscope",
+    version = "*",
+    event = { "BufReadPost" },
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "alpha",
+          "dashboard",
+          "fzf",
+          "help",
+          "lazy",
+          "lazyterm",
+          "mason",
+          "neo-tree",
+          "notify",
+          "toggleterm",
+          "Trouble",
+          "trouble",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
 }
