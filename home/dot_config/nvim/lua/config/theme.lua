e366c235aeb00fa92a0a579f2737c370de97e7b5
d-issy <12374694+d-issy@users.noreply.github.com>
2024-06-16 21:26:42 +0900

    ...

--- home/dot_config/nvim/lua/config/theme.lua
+++ home/dot_config/nvim/lua/config/theme.lua
@@ -1,17 +1,69 @@
+-- komau color pallet
 local c = {
-  bg = "#212529",
-
-  p1 = "#f8f9fa",
-  p2 = "#ced4da",
-  p3 = "#6c757d",
-
-  blue = "#a3cef1",
+  black = "#222222",
+  medium_gray = "#767676",
+  white = "#F5EEE6",
+  light_black = "#424242",
+  lighter_black = "#545454",
+  subtle_black = "#303030",
+  light_gray = "#999999",
+  lighter_gray = "#CCCCCC",
+  lightest_gray = "#E5E5E5",
+  yellow = "#FED442",
+  pink = "#D73A49",
+  green = "#22863A",
+  red = "#B31D28",
+  orange = "#E36209",
+  blue = "#005CC5",
 }
 
-local hls = {}
+local hl = {
+  Normal = { fg = c.white, bg = c.black },
+  Cursor = { fg = c.lighter_gray, bg = c.lighter_black },
+  Comment = { fg = c.lighter_black },
+
+  Constant = { fg = c.medium_gray },
+  Character = { fg = c.medium_gray },
+  Number = { fg = c.medium_gray },
+  Boolean = { fg = c.medium_gray },
+  Float = { fg = c.medium_gray },
+  String = { fg = c.medium_gray },
+
+  Identifier = { fg = c.white },
+  Function = { fg = c.white },
+
+  Statement = { fg = c.pink },
+  Conditional = { fg = c.pink },
+  Repeat = { fg = c.pink },
+  Label = { fg = c.pink },
+  Keyword = { fg = c.pink },
+  Exception = { fg = c.pink },
+
+  Operator = { fg = c.light_gray },
+
+  PreProc = { fg = c.medium_gray },
+  Include = { fg = c.medium_gray },
+  Define = { fg = c.medium_gray },
+  Macro = { fg = c.medium_gray },
+  Precondit = { fg = c.medium_gray },
+
+  Type = { fg = c.white },
+  StorageClass = { fg = c.white },
+  Structure = { fg = c.white },
+  Typedef = { fg = c.white },
+
+  Special = { fg = c.medium_gray },
+  SpecialChar = { fg = c.medium_gray },
+  Tag = { fg = c.medium_gray },
+  Delimiter = { fg = c.medium_gray },
+  SpecialComment = { fg = c.medium_gray },
+  Debug = { fg = c.medium_gray },
+
+  WinBar = { fg = c.white, bg = c.black },
+}
 
 -- colorschme load
 vim.cmd "hi clear"
-for group, opts in pairs(hls) do
-  vim.api.nvim_set_hl(0, group, { bg = opts.bg, fg = opts.fg })
+for group, opts in pairs(hl) do
+  vim.api.nvim_set_hl(0, group, opts)
 end
