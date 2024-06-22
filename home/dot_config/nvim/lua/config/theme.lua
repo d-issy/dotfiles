bcebac5abadda8f15f4794a965a7b5ebe3055ad4
d-issy <12374694+d-issy@users.noreply.github.com>
2024-06-23 02:56:31 +0900

    update theme

--- home/dot_config/nvim/lua/config/theme.lua
+++ home/dot_config/nvim/lua/config/theme.lua
@@ -1,26 +1,31 @@
 local palette = {
   bg = "#272727",
-  default = "#acacac",
+  default = "#9c9c9c",
+
+  border = "#ffffff",
+
+  ui = "#cfcfcf",
+  visual_bg = "#383838",
+
   low = "#797979",
   search = "#f2f2aa",
+  cmp = "#f9f9f9",
 
-  string = "#a6da95",
   error = "#ed8796",
   warning = "#eed49f",
   info = "#7dc4e4",
   hint = "#8aadf4",
 
-  visual = "#ffffff",
-  visual_bg = "#383838",
+  folder = "#54aeff",
 }
 
 local hl = {
-  -- reset colors
   Normal = { fg = palette.default, bg = palette.bg },
-  WinBar = { fg = palette.default, bg = palette.bg },
-  Comment = { fg = palette.low },
 
-  Visual = { fg = palette.visual, bg = palette.visual_bg },
+  WinBar = { fg = palette.ui, bg = palette.bg },
+  WinBarNC = { link = "WinBar" },
+  Comment = { fg = palette.low },
+  Visual = { bg = palette.visual_bg },
 
   Identifier = { fg = palette.default },
   Function = { fg = palette.default },
@@ -32,30 +37,61 @@ local hl = {
   Type = { fg = palette.default },
   Todo = { fg = palette.default },
 
-  Search = { fg = palette.search, bg = palette.bg },
-  CurSearch = { fg = palette.search, bg = palette.bg, underline = true },
-  ["@variable"] = { fg = palette.default },
-
-  -- set
   Special = { fg = palette.low },
   Operator = { fg = palette.low },
-  ["@punctuation.bracket"] = { fg = palette.low },
-  ["@punctuation.delimiter"] = { fg = palette.low },
+  Search = { fg = palette.search, bg = palette.bg },
+  CurSearch = { fg = palette.search, bg = palette.bg, underline = true },
+
+  -- set
 
   DiagnosticError = { fg = palette.error },
   DiagnosticWarn = { fg = palette.warning },
   DiagnosticInfo = { fg = palette.info },
   DiagnosticHint = { fg = palette.hint },
 
-  DiagnosticUnnecessary = { fg = palette.hint, underline = true },
-  DiagnosticUnderlineError = { fg = palette.error, underline = true },
+  DiagnosticUnderlineError = { undercurl = true, sp = palette.error },
+  DiagnosticUnderlineWarn = { undercurl = true, sp = palette.warning },
+  DiagnosticUnderlineInfo = { undercurl = true, sp = palette.info },
+  DiagnosticUnderlineHint = { undercurl = true, sp = palette.hint },
+  DiagnosticUnnecessary = { link = "DiagnosticUnderlineHint" },
+
+  -- lsp
+  ["@punctuation"] = { link = "Special" },
+  ["@variable"] = { link = "Variable" },
 
   -- sql
   ["@type.builtin.sql"] = { fg = palette.default },
+
+  -------------
+  -- plugins --
+  -------------
+
+  -- DropBar
+  Directory = { fg = palette.folder },
+
+  -- MiniFiles
+  MiniFilesDirectory = { fg = palette.folder },
+  MiniFilesNormal = { fg = palette.ui },
+  MiniFilesBorder = { fg = palette.border },
+  MiniFilesCursorLine = { bg = palette.visual_bg },
+
+  -- Telescope
+  TelescopeNormal = { fg = palette.ui, bg = palette.bg },
+  TelescopeBorder = { fg = palette.border },
+  TelescopeSelection = { bg = palette.visual_bg },
 }
 
 -- colorschme load
-vim.cmd "hi clear"
+if vim.g.colors_name then
+  vim.cmd "hi clear"
+  if package.loaded["nvim-web-devicons"] then
+    -- require("nvim-web-devicons").refresh()
+  end
+end
+
+vim.g.termguicolors = true
+vim.g.colors_name = "mytheme"
+
 for group, opts in pairs(hl) do
   vim.api.nvim_set_hl(0, group, opts)
 end
