672cdf84681be2bb3b377c6eaa1b44e1f82209ed
d-issy <12374694+d-issy@users.noreply.github.com>
2024-06-30 21:04:53 +0900

    add keybind

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -10,11 +10,20 @@ map.setup {
   { "<leader>md", "<cmd>delmarks! | delmarks A-Z0-9<cr>", desc = "Delete All Marks" },
   { "<leader>bd", buffer.remove, desc = "Buffer Remove" },
   { "<esc>", "<cmd>noh<cr><esc>", desc = "esc", mode = { "i", "n" } },
+  { "n", "nzz", desc = "Next" },
+  { "N", "Nzz", desc = "Previous" },
+
+  -- resize
+  { "<c-up>", "<cmd>resize +2<cr>", mode = { "n", "t" }, desc = "Resize Up" },
+  { "<c-down>", "<cmd>resize -2<cr>", mode = { "n", "t" }, desc = "Resize Down" },
+  { "<c-left>", "<cmd>vertical resize -2<cr>", mode = { "n", "t" }, desc = "Resize Left" },
+  { "<c-right>", "<cmd>vertical resize +2<cr>", mode = { "n", "t" }, desc = "Resize Right" },
 
   -- terminal
   { "<leader><tab><tab>", "10<c-w>s<cmd>terminal<cr>", desc = "Terminal" },
   { "<leader><tab>v", "<c-w>v<cmd>terminal<cr>", desc = "Terminal" },
   { "<leader><esc>", "<c-\\><c-n>", mode = "t", desc = "ESC" },
+  { "<c-w>c", "<c-\\><c-n><c-w>c", mode = "t", desc = "Close" },
   { "<c-w>h", "<c-\\><c-n><c-w>h", mode = "t", desc = "Left" },
   { "<c-w>j", "<c-\\><c-n><c-w>j", mode = "t", desc = "Down" },
   { "<c-w>k", "<c-\\><c-n><c-w>k", mode = "t", desc = "Up" },
