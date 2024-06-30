afdc60e6f74e2bd214979c7c32c4f69ed25b612a
d-issy <12374694+d-issy@users.noreply.github.com>
2024-06-30 20:39:58 +0900

    noremap

--- home/dot_config/nvim/lua/util/map.lua
+++ home/dot_config/nvim/lua/util/map.lua
@@ -15,7 +15,12 @@ local array = require "util.array"
 function M.setup(keys, default_opts)
   for _, key in ipairs(keys) do
     local mode = key.mode and array.new(key.mode) or { "n" }
-    local opts = vim.tbl_deep_extend("force", { silent = true, desc = key.desc }, default_opts or {}, key[3] or {})
+    local opts = vim.tbl_deep_extend("force", {
+      silent = true,
+      noremap = true,
+      desc = key.desc,
+    }, default_opts or {}, key[3] or {})
+
     vim.keymap.set(mode, key[1], key[2], opts)
   end
 end
