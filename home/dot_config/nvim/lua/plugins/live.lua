0676b0a7feb6cd7b7876cfacf9c7ed67464cb0d8
issy <12374694+d-issy@users.noreply.github.com>
2024-07-01 21:03:29 +0900

    Add ScreenKey (#203)

---
+++ home/dot_config/nvim/lua/plugins/live.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "NStefan002/screenkey.nvim",
+    version = "*",
+    opts = {
+      win_opts = {
+        zindex = 1000,
+      },
+    },
+    keys = {
+      { "<leader>uk", "<cmd>Screenkey<cr>", desc = "Screenkey" },
+    },
+  },
+}
