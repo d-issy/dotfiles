9fc8594eafaa3116a076fa086edbfcdfe51c504f
d-issy <12374694+d-issy@users.noreply.github.com>
2024-07-02 07:55:02 +0900

    feat: Wrap git status update in vim.schedule
    This commit wraps the git status update logic in a vim.schedule function.
    This is done to ensure that the UI updates are scheduled correctly and
    do not block the main thread. This change improves the responsiveness
    of the UI when there are changes in the git status.

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -32,48 +32,50 @@ return {
         group = augroup "status",
         pattern = "MiniFilesBufferUpdate",
         callback = function(args)
-          ---@type table<string, {text: string, hl: string}>
-          local sign = {}
+          vim.schedule(function()
+            ---@type table<string, {text: string, hl: string}>
+            local sign = {}
 
-          -- git
-          local git = require "util.git"
-          local git_root = git.root()
-          if git_root ~= nil then
-            local status = git.get_status(git_root)
-            for path, flags in pairs(status) do
-              if flags.unstaged == git.FLAGS.RENAMED then
-                sign[path] = { text = "~", hl = "MiniFilesChange" }
-              elseif flags.unstaged == git.FLAGS.MODIFIED then
-                sign[path] = { text = "~", hl = "MiniFilesChange" }
-              elseif flags.unstaged == git.FLAGS.UNTRACKED then
-                sign[path] = { text = "+", hl = "MiniFilesAdd" }
+            -- git
+            local git = require "util.git"
+            local git_root = git.root()
+            if git_root ~= nil then
+              local status = git.get_status(git_root)
+              for path, flags in pairs(status) do
+                if flags.unstaged == git.FLAGS.RENAMED then
+                  sign[path] = { text = "~", hl = "MiniFilesChange" }
+                elseif flags.unstaged == git.FLAGS.MODIFIED then
+                  sign[path] = { text = "~", hl = "MiniFilesChange" }
+                elseif flags.unstaged == git.FLAGS.UNTRACKED then
+                  sign[path] = { text = "+", hl = "MiniFilesAdd" }
+                end
               end
             end
-          end
 
-          -- file changes (not saved)
-          for _, change in ipairs(vim.fn.getbufinfo { bufmodified = 1 }) do
-            sign[change.name] = { text = "*", hl = "MiniFilesChange" }
-          end
-
-          local buf = args.data.buf_id
-          local lines = vim.api.nvim_buf_line_count(buf)
-          for i = 1, lines do
-            local entry = mini_files.get_fs_entry(buf, i)
-            if not entry then
-              return
+            -- file changes (not saved)
+            for _, change in ipairs(vim.fn.getbufinfo { bufmodified = 1 }) do
+              sign[change.name] = { text = "*", hl = "MiniFilesChange" }
             end
 
-            local path = entry.path
-            if sign[path] then
-              vim.api.nvim_buf_set_extmark(buf, ns_mini_files_git, i - 1, 0, {
-                sign_text = sign[path].text,
-                sign_hl_group = sign[path].hl,
-                priority = 2,
-                invalidate = true,
-              })
+            local buf = args.data.buf_id
+            local lines = vim.api.nvim_buf_line_count(buf)
+            for i = 1, lines do
+              local entry = mini_files.get_fs_entry(buf, i)
+              if not entry then
+                return
+              end
+
+              local path = entry.path
+              if sign[path] then
+                vim.api.nvim_buf_set_extmark(buf, ns_mini_files_git, i - 1, 0, {
+                  sign_text = sign[path].text,
+                  sign_hl_group = sign[path].hl,
+                  priority = 2,
+                  invalidate = true,
+                })
+              end
             end
-          end
+          end)
         end,
       })
     end,
