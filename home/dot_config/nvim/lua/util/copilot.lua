9259506b6e68f7122f4fca2ba804926556d7c55e
issy <12374694+d-issy@users.noreply.github.com>
2024-07-05 01:00:47 +0900

    disable copilot when using snippet (#212)

---
+++ home/dot_config/nvim/lua/util/copilot.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+--- Check if copilot is enabled
+--- @return boolean
+function M.enabled()
+  if package.loaded["copilot"] == nil then
+    return false
+  end
+  return vim.g.copilot == nil or vim.g.copilot
+end
+
+--- Toggle copilot
+function M.toggle()
+  vim.g.copilot = not vim.g.copilot
+end
+
+--- Enable copilot
+function M.on()
+  vim.g.copilot = true
+end
+
+--- Disable copilot
+function M.off()
+  vim.g.copilot = false
+end
+
+return M
