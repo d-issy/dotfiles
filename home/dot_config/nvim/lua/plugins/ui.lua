dfe970d660f4d277b08e32587edc6d463f35e36d
issy <12374694+d-issy@users.noreply.github.com>
2024-07-20 00:23:56 +0900

    feat: update diagnostic config and remove redundant setting (#242)

--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -7,7 +7,6 @@ return {
     opts = {
       options = {
         diagnostics = "nvim_lsp",
-        diagnostics_update_in_insert = true,
         ---@diagnostic disable-next-line: unused-local
         diagnostics_indicator = function(count, level, diagnostics_dict, context)
           local icon = level:match "error" and " " or " "
