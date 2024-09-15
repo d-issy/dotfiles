ad1f0da670997fe602b77ee1cf705ff36217ae7e
d-issy <12374694+d-issy@users.noreply.github.com>
2024-09-16 08:30:02 +0900

    refactor: Move navi cheats and config to navi.nix

--- modules/navi.nix
+++ modules/navi.nix
@@ -1,5 +1,34 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+with lib;
+
+let
+  yamlFormat = pkgs.formats.yaml { };
+  settings = {
+    style = {
+      tag = {
+        color = "cyan";
+        with_percentage = 26;
+        min_width = 20;
+      };
+      comments = {
+        color = "blue";
+        with_percentage = 42;
+        min_width = 45;
+      };
+      snippet.color = "white";
+    };
+    finder = {
+      command = "fzf";
+      overrides = "--no-exact";
+      overrides_var = "--no-exact --no-select-1";
+    };
+    cheats.paths = [
+      "~/cheats/"
+    ];
+    shell.command = "zsh";
+  };
+in
 {
   config = {
     home.packages = [ pkgs.navi ];
@@ -8,8 +37,12 @@
       eval "$(${pkgs.navi}/bin/navi widget zsh)"
     '';
 
-    xdg.configFile."navi" = {
-      source = ../files/navi;
+    xdg.configFile."navi/config.yaml" = mkIf (settings != { }) {
+      source = yamlFormat.generate "navi-config" settings;
+    };
+
+    home.file."cheats" = {
+      source = ../files/cheats;
       recursive = true;
     };
   };
