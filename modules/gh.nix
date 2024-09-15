498ce57977b5d2bd8be91a15d3c88046efedde14
d-issy <12374694+d-issy@users.noreply.github.com>
2024-09-15 16:23:34 +0900

    feat(gh.nix): Add aliases and extensions to gh.nix

--- modules/gh.nix
+++ modules/gh.nix
@@ -6,6 +6,16 @@
     settings = {
       git_protocol = "ssh";
       prefer_editor_prompt = "enabled";
+      aliases = {
+        prs = "f prs";
+        grep = "f greps";
+      };
     };
+    extensions = [
+      pkgs.gh-dash
+      pkgs.gh-f
+      pkgs.gh-notify
+      pkgs.gh-poi
+    ];
   };
 }
