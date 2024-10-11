b613a32154366e72e8501a9da2a55cb735b6bf4a
d-issy <12374694+d-issy@users.noreply.github.com>
2024-10-11 23:47:46 +0900

    feat(zsh.nix): add vi-mode plugin to zsh configuration

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -20,6 +20,13 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    plugins = [
+      {
+        name = "vi-mode";
+        src = pkgs.zsh-vi-mode;
+        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+      }
+    ];
 
     # alias
     shellAliases = {
