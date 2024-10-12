6097fa9509b8a53aae2461cd84c523d7462dd6b1
d-issy <12374694+d-issy@users.noreply.github.com>
2024-10-13 07:09:53 +0900

    feat(nushell): enhance nixshell vi keybind configuration

--- modules/nushell.nix
+++ modules/nushell.nix
@@ -8,11 +8,24 @@ in
     programs.nushell = {
       enable = true;
 
-      shellAliases = home.shellAliases;
-      environmentVariables = lib.attrsets.mapAttrs (name: value: ''"${value}"'') (home.sessionVariables // { });
+      shellAliases = home.shellAliases // {
+        ll = "ls -l";
+        la = "ls -la";
+      };
+
+      environmentVariables = lib.attrsets.mapAttrs (name: value: ''"${value}"'') (home.sessionVariables // {
+        # disable indicator vi
+        PROMPT_INDICATOR_VI_NORMAL = "";
+        PROMPT_INDICATOR_VI_INSERT = "";
+      });
 
       extraConfig = ''
         $env.config.show_banner = false
+        $env.config.edit_mode = "vi"
+
+        $env.config.ls.use_ls_colors = false
+        $env.config.cursor_shape.vi_insert = "line"
+        $env.config.cursor_shape.vi_normal = "block"
       '';
     };
   };
