5ea0f6f37f24c0de7e3d80ee4bb200db27d046bd
d-issy <12374694+d-issy@users.noreply.github.com>
2024-11-01 01:45:15 +0900

    feat(nushell): add pre_prompt hook configuration

--- modules/mise.nix
+++ modules/mise.nix
@@ -1,4 +1,6 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+
+with lib;
 
 {
   config = {
@@ -32,6 +34,34 @@
           missing_tools = "never";
         };
       };
+    };
+    programs.nushell = {
+      extraConfig = mkAfter ''
+        $env.config = ($env.config? | default {})
+        $env.config.hooks = ($env.config.hooks? | default {})
+        $env.config.hooks.pre_prompt = (
+          $env.config.hooks.pre_prompt?
+          | default []
+          | prepend {
+              let ops = (
+                ${pkgs.mise}/bin/mise hook-env -s nu
+                | from csv --noheaders
+                | rename 'op' 'name' 'value'
+              )
+              if ($ops | is-empty) {
+                return
+              }
+              for $it in $ops {
+                if $it.op == "set" {
+                  let value = if $it.name == "PATH" { $it.value | split row (char esep) | uniq } else { $it.value }
+                  load-env {($it.name): $value}
+                } else if $it.op == "hide" {
+                  hide-env $it.name
+                }
+              }
+          }
+        )
+      '';
     };
   };
 }
