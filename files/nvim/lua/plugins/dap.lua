1b47de63dc48f838a64f4c7fc7081b2d018e1889
issy <12374694+d-issy@users.noreply.github.com>
2024-11-11 09:56:17 +0900

    add nvim-dap configuration for debugging (#335)
    This pull request adds a new configuration for the `nvim-dap` plugin in
    the `files/nvim/lua/plugins/dap.lua` file, including dependencies, key
    mappings, and setup for Go language debugging.
    
    Key changes include:
    
    ### Plugin Configuration
    * Added the `nvim-dap` plugin along with its dependencies:
    `nvim-dap-ui`, `nvim-nio`, and `nvim-dap-go`.
    
    ### Key Mappings
    * Defined various key mappings for debugging actions such as setting
    breakpoints, continuing execution, stepping into and over code, pausing,
    and toggling the REPL.
    
    ### Setup and Configuration
    * Configured the `dap` and `dapui` plugins to automatically open and
    close the UI on certain debugging events.
    * Set up Go language debugging configurations, including remote
    attachment and delve settings.

---
+++ files/nvim/lua/plugins/dap.lua
@@ -0,0 +1,56 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    -- languages
+    "leoluz/nvim-dap-go",
+  },
+    -- stylua: ignore
+    keys = {
+      { "<leader>d", "", desc = "+debug", mode = {"n", "v"} },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+      { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+      { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+      -- ui
+      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+      { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+    },
+  config = function()
+    local dap = require "dap"
+
+    -- ui
+    local dapui = require "dapui"
+    dapui.setup()
+
+    -- languages
+    require("dap-go").setup {}
+    require("dap.ext.vscode").load_launchjs()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+  end,
+}
