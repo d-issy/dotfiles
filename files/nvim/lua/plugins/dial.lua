3eccf8c9cb8765364de7a3fb671ea8fe3260d8f0
issy <12374694+d-issy@users.noreply.github.com>
2024-12-24 07:40:00 +0900

    Add dial.nvim (#354)

---
+++ files/nvim/lua/plugins/dial.lua
@@ -0,0 +1,98 @@
+local M = {}
+
+---@param increment boolean
+---@param g? boolean
+function M.dial(increment, g)
+  local mode = vim.fn.mode(true)
+  -- Use visual commands for VISUAL 'v', VISUAL LINE 'V' and VISUAL BLOCK '\22'
+  local is_visual = mode == "v" or mode == "V" or mode == "\22"
+  local func = (increment and "inc" or "dec") .. (g and "_g" or "_") .. (is_visual and "visual" or "normal")
+  local group = vim.g.dials_by_ft[vim.bo.filetype] or "default"
+  return require("dial.map")[func](group)
+end
+
+return {
+  "monaqa/dial.nvim",
+  -- stylua: ignore
+  keys = {
+    { "<C-a>", function() return M.dial(true) end, expr = true, desc = "Increment", mode = {"n", "v"} },
+    { "<C-x>", function() return M.dial(false) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
+    { "g<C-a>", function() return M.dial(true, true) end, expr = true, desc = "Increment", mode = {"n", "v"} },
+    { "g<C-x>", function() return M.dial(false, true) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
+  },
+  opts = function()
+    local augend = require "dial.augend"
+
+    local logical_alias = augend.constant.new { elements = { "&&", "||" }, word = false, cyclic = true }
+    local capitalized_boolean = augend.constant.new { elements = { "True", "False" }, word = true, cyclic = true }
+
+    return {
+      dials_by_ft = {
+        css = "css",
+        vue = "vue",
+        javascript = "typescript",
+        typescript = "typescript",
+        typescriptreact = "typescript",
+        javascriptreact = "typescript",
+        json = "json",
+        lua = "lua",
+        markdown = "markdown",
+        sass = "css",
+        scss = "css",
+        python = "python",
+      },
+      groups = {
+        default = {
+          augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
+          augend.integer.alias.decimal_int, -- nonnegative and negative decimal number
+          augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+          augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
+          augend.date.alias["%Y-%m-%d"], -- date (2022-02-19, etc.)
+          capitalized_boolean,
+          augend.constant.alias.bool, -- boolean value (true <-> false)
+          logical_alias,
+        },
+        css = {
+          augend.hexcolor.new { case = "lower" },
+          augend.hexcolor.new { case = "upper" },
+        },
+        vue = {
+          augend.constant.new { elements = { "let", "const" } },
+          augend.hexcolor.new { case = "lower" },
+          augend.hexcolor.new { case = "upper" },
+        },
+        typescript = {
+          augend.constant.new { elements = { "let", "const" } },
+        },
+        markdown = {
+          augend.misc.alias.markdown_header,
+        },
+        json = {
+          augend.semver.alias.semver, -- versioning (v1.1.2)
+        },
+        lua = {
+          augend.constant.new {
+            elements = { "and", "or" },
+            word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+            cyclic = true, -- "or" is incremented into "and".
+          },
+        },
+        python = {
+          augend.constant.new {
+            elements = { "and", "or" },
+          },
+        },
+      },
+    }
+  end,
+  config = function(_, opts)
+    -- copy defaults to each group
+    for name, group in pairs(opts.groups) do
+      if name ~= "default" then
+        vim.list_extend(group, opts.groups.default)
+      end
+    end
+    require("dial.config").augends:register_group(opts.groups)
+    vim.g.dials_by_ft = opts.dials_by_ft
+  end,
+}
