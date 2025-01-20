6743a5224218953ade01fe1831c34bf51eb18e40
d-issy <12374694+d-issy@users.noreply.github.com>
2025-01-21 01:50:02 +0900

    feat: replace fzf-lua with snacks.nvim picker

--- files/nvim/lua/plugins/todo.lua
+++ files/nvim/lua/plugins/todo.lua
@@ -1,13 +1,10 @@
 return {
   "folke/todo-comments.nvim",
-  dependencies = { "fzf-lua" },
   event = { "BufReadPost" },
   opts = {},
   -- stylua: ignore
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-    { "<leader>ft", function() require("todo-comments.fzf").todo() end, desc = "Todo" },
-    { "<leader>fT", function () require("todo-comments.fzf").todo { keywords = {"TODO", "FIX", "FIXME"} } end, desc = "Todo/Fix/Fixme" },
   },
 }
