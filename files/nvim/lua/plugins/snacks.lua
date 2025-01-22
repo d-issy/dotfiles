3ed96258563be16090df2199bb21f1d94f4be3e5
d-issy <12374694+d-issy@users.noreply.github.com>
2025-01-23 00:23:03 +0900

    feat: replace fzf-lua with snacks-picker and update LSP progress notifications

--- files/nvim/lua/plugins/snacks.lua
+++ files/nvim/lua/plugins/snacks.lua
@@ -102,5 +102,21 @@ return {
     snacks.toggle.diagnostics():map "<leader>ud"
     snacks.toggle.line_number():map "<leader>un"
     snacks.toggle.indent():map "<leader>ug"
+
+    vim.api.nvim_create_autocmd("LspProgress", {
+      ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+      callback = function(ev)
+        local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+        vim.notify(vim.lsp.status(), vim.log.levels.INFO, {
+          id = "lsp_progress",
+          title = "LSP Progress",
+          ---@diagnostic disable: undefined-field
+          opts = function(notif)
+            notif.icon = ev.data.params.value.kind == "end" and " "
+              or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+          end,
+        })
+      end,
+    })
   end,
 }
