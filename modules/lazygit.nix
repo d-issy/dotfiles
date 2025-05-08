c16a2eea53cca2458dcf1a6aea10f64ca23b3dd2
issy <12374694+d-issy@users.noreply.github.com>
2025-05-08 09:40:11 +0900

    modules/lazygit.nix: group GUI settings under gui and add git.autoForwardBranches (#395)

--- modules/lazygit.nix
+++ modules/lazygit.nix
@@ -7,23 +7,28 @@
     programs.lazygit = {
       enable = true;
       settings = {
-        gui.language = "en";
-        gui.nerdFontsVersion = "3";
-        gui.sidePanelWidth = 0.30;
-        gui.expandFocusedSidePanel = true;
-        gui.commitLogSize = 2;
-        gui.showUntrackedFiles = true;
-        gui.authorColors = { "*" = "#b7bdf8"; };
-        gui.theme = {
-          activeBorderColor = [ "#f5a97f" "bold" ];
-          inactiveBorderColor = [ "#494d64" ];
-          optionsTextColor = [ "#8aadf4" ];
-          selectedLineBgColor = [ "#363a4f" ];
-          cherryPickedCommitBgColor = [ "#363a4f" ];
-          cherryPickedCommitFgColor = [ "#cad3f5" ];
-          unstagedChangesColor = [ "#ed8796" ];
-          defaultFgColor = [ "#cad3f5" ];
-          searchingActiveBorderColor = [ "#eed49f" ];
+        gui = {
+          language = "en";
+          nerdFontsVersion = "3";
+          sidePanelWidth = 0.30;
+          expandFocusedSidePanel = true;
+          commitLogSize = 2;
+          showUntrackedFiles = true;
+          authorColors = { "*" = "#b7bdf8"; };
+          theme = {
+            activeBorderColor = [ "#f5a97f" "bold" ];
+            inactiveBorderColor = [ "#494d64" ];
+            optionsTextColor = [ "#8aadf4" ];
+            selectedLineBgColor = [ "#363a4f" ];
+            cherryPickedCommitBgColor = [ "#363a4f" ];
+            cherryPickedCommitFgColor = [ "#cad3f5" ];
+            unstagedChangesColor = [ "#ed8796" ];
+            defaultFgColor = [ "#cad3f5" ];
+            searchingActiveBorderColor = [ "#eed49f" ];
+          };
+          git = {
+            autoForwardBranches = "none";
+          };
         };
         quitOnTopLevelReturn = true;
         keybinding = {
