190d87b1bf6fcfb4fe9fdb9ab3b96cee821c8ead
d-issy <12374694+d-issy@users.noreply.github.com>
2026-05-17 15:36:46 +0900

    refactor(neovim): move git plugin config to nixvim
    - replace Lua gitsigns setup with native Nix module
    - fold lazy.nvim setup into plugin defaults module

--- modules/neovim/plugins/default.nix
+++ modules/neovim/plugins/default.nix
@@ -1,6 +1,33 @@
+{ pkgs, ... }:
+
 {
   imports = [
-    ./lazy.nix
+    ./git.nix
     ./oil.nix
   ];
+
+  programs.nixvim = {
+    plugins.lz-n.enable = true;
+
+    extraPlugins = [
+      pkgs.vimPlugins.lazy-nvim
+    ];
+
+    extraConfigLua = ''
+      require("lazy").setup({
+        defaults = { lazy = true },
+        ui = { border = "rounded" },
+        lockfile = vim.env.DOTFILES_DIR .. "/files/nvim/lazy-lock.json",
+        performance = {
+          reset_packpath = false,
+          rtp = {
+            reset = false,
+          },
+        },
+        spec = {
+          { import = "plugins" },
+        },
+      })
+    '';
+  };
 }
