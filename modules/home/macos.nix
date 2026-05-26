2c8709886c7ab82e0385b76e7d29c276de4a837f
issy <12374694+d-issy@users.noreply.github.com>
2026-05-26 15:21:44 +0900

    Disable Darwin app management (#528)
    ## Summary
    
    - Disable Home Manager Darwin app copy/link handling for macOS.
    
    ## Testing
    
    - Not run (configuration-only change).

--- modules/home/macos.nix
+++ modules/home/macos.nix
@@ -4,5 +4,8 @@
   home.username = "issy";
   home.homeDirectory = "/Users/issy";
 
+  targets.darwin.copyApps.enable = false;
+  targets.darwin.linkApps.enable = false;
+
   imports = [ ../recipes ];
 }
