bd58622aa3d15db0cce625aad112269e5772d299
d-issy <12374694+d-issy@users.noreply.github.com>
2026-06-05 11:20:01 +0900

    Add worktree aliases and cheat entry

--- modules/recipes/scripts.nix
+++ modules/recipes/scripts.nix
@@ -1,4 +1,18 @@
+{ pkgs, ... }:
+
 {
+  home.shellAliases = {
+    wt = "worktree";
+    wtc = "worktree create";
+    wtcf = "worktree create --finish";
+    wtd = "worktree delete";
+    wtdc = "worktree delete --current";
+    wtl = "worktree list";
+    wtr = "worktree rename";
+    wts = "worktree status";
+    wtsw = "worktree switch";
+  };
+
   dot.programs.scripts = {
     enable = true;
     gitAutoCommit = {
@@ -10,4 +24,21 @@
       thinking = "low";
     };
   };
+
+  dot.programs.navi.cheats.worktree.sections = [
+    {
+      tags = [ "worktree" ];
+      variables = {
+        pull_number = "${pkgs.gh}/bin/gh pr list --- --column 1 --delimiter '\\t'";
+        worktree_query = "${pkgs.git}/bin/git worktree list --porcelain | ${pkgs.gawk}/bin/awk '/^branch / { sub(\"refs/heads/\", \"\", $2); print $2 }'";
+      };
+      entries = [
+        {
+          description = "Switch GitHub pull request worktree";
+          alias = "wtpr";
+          command = "worktree switch --pr <pull_number>";
+        }
+      ];
+    }
+  ];
 }
